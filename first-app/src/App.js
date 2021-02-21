bde8279e1e08c3beca3581b60123165b204cc9b8
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-02-21 15:41:30 +0900

    [add]hello world

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -5,16 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello React
-        </a>
+
+          <h1>Hello world</h1>
+        
       </header>
     </div>
   );
