9de6e3249e42ece1b3c4f16d99707f721aa40c13
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-02-21 21:18:22 +0900

    [add]いろち出来た

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -5,19 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-      <script type="text/javascript">
-        
-        function disp(){
-          window.alert("ちわ～")
-        }
-
-      </script>
 
 
 
-      <p><input type="button" value="Hello world" onClick="disp()"></input>
-      </p>
+
+
 
 
         
