af33b589e19b6b51293faf34f797abaf105eb3b9
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-02-28 10:12:44 +0900

    [add]20210228

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,18 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const hc = () => {
+    window.alert('Hello world')
+  }
+  const name = 'eriko';
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+  const yokiyoki = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+  const badB = () => {
+    setCount(count - 1);
+    console.log(count);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
 
+        <button
+          onClick={hc}> Hello world!</button>
+
+
+        <div>
+          <h1>Hello world</h1>
+          <h2>Hello world</h2>
+          <h3>Hello world</h3>
+          <h4>Hello world</h4>
+
+          <button>button</button>
+
+          <button
+            onClick={() => window.alert("Hello world2")}
+          >button2</button>
+
+          <button onClick={increment}>up</button>
+          <button onClick={yokiyoki}>いいね。</button>
+          <button onClick={badB}>よくないね。</button>
+
+          <input type="text" />
+          <a href="#">a タグ</a>
+          {name}
+        </div>
 
 
 
 
-
-
-        
       </header>
     </div>
   );
