db48b726f24aa8cae94e29797043f38b2afe7903
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-03-14 18:29:31 +0900

    [add]EXTRA #って。

--- first-app/src/ComponentA.js
+++ first-app/src/ComponentA.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table } from 'react-bootstrap';
-import { Card } from 'react-bootstrap';
-// import Card from './Card';
+// import { Card } from 'react-bootstrap';
+import Card from './Card';
 // ↑これを復活させて上の｛card｝を消すとtableができるが、２つは無理なのか？
 
 
@@ -86,35 +86,7 @@ const ComponentA = () => {
 
                 </tbody>
             </Table>
-            <Table striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </Table>
+
         </>
 
     )
