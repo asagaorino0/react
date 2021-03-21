d19ba8d26e710d3b8a8b36927ab144b70215e4ad
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-03-21 22:17:09 +0900

    [add]05.EXTRA

--- first-app/src/components/ComponentC.js
+++ first-app/src/components/ComponentC.js
@@ -3,37 +3,14 @@ import { Link } from 'react-router-dom';
 
 const ComponentC = () => {
 
-
-    // class Hello extends React.Component {
-    //     constructor(props) {
-    //         super(props);
-    //         this.state = { msg: 'Hello!' };
-    //     }
-
-    //     render() {
-    //         return (
-    //             <div>
-    //                 <h1>{this.state.msg}</h1>
-    //                 <button onClick={() => this.setState({ msg: 'Bye!' })}>Click</button>
-    //             </div>
-    //         );
-    //     }
-    // }
-
     return (
         <>
 
             <div>ComponentC</div>
             <Link to="/">ComponentAへ移動</Link>
 
-
-
         </>
     )
-
-
-
-
 }
 
 export default ComponentC
