1336fb0150fd3ba5d69bb804d3e4393c2005a11c
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-03-21 22:12:06 +0900

    [add]05.EXTRA

--- first-app/src/components/ComponentA.js
+++ first-app/src/components/ComponentA.js
@@ -3,10 +3,13 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 
 const ComponentA = () => {
     const [count, setCount] = useState(0);
     const [data, setData] = useState([]);
+    const [display, setDisplay] = useState(false)
+
     useEffect(() => {
         console.log('useEfefectが呼び出されました。');
 
@@ -15,16 +18,19 @@ const ComponentA = () => {
                 setData(res.data)
                 console.log(res.data, 'res check')
             })
-    }, []);
+    }, false);
 
     const increment = () => {
         setCount(count + 1);
     };
 
 
+
     return (
         <>
             <div>ComponentA</div>
+
+            <Button onClick={() => setDisplay(!display)}>Get!</Button>{' '}
             <Table striped bordered hover>
                 <thead>
                     <tr>
@@ -34,20 +40,22 @@ const ComponentA = () => {
                         <th>body</th>
                     </tr>
                 </thead>
-                <tbody>
-                    {
-                        data.map(element => {
-                            return (
-                                <tr>
-                                    <td>{element.userId}</td>
-                                    <td>{element.id}</td>
-                                    <td>{element.title}</td>
-                                    <td>{element.body}</td>
-                                </tr>
-                            );
-                        })
-                    }
-                </tbody>
+                {display &&
+                    <tbody>
+                        {
+                            data.map(element => {
+                                return (
+                                    <tr>
+                                        <td>{element.userId}</td>
+                                        <td>{element.id}</td>
+                                        <td>{element.title}</td>
+                                        <td>{element.body}</td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>}
+
             </Table>
 
             <Link to="componentb">ComponentBへ移動</Link>
